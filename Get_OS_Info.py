f402132119369e5796c9c747ae072d76e55a3114
Lichard <lichard.baliuag@greetify.co.nz>
2023-05-22 17:52:45 +1200

    print unumber

--- Get_OS_Info.py
+++ Get_OS_Info.py
@@ -11,11 +11,11 @@ used = os.times()
 now = time.time()
 means = time.ctime(now)
 
-print "User number",unumber
-print "Process ID",pnumber
-print "Current Directory",where
-print "System information",what
-print "System information",used
-
-print "\nTime is now",now
-print "Which interprets as",means
+print("User number",unumber)
+#print "Process ID",pnumber
+#print "Current Directory",where
+#print "System information",what
+#print "System information",used
+#
+#print "\nTime is now",now
+#print "Which interprets as",means
